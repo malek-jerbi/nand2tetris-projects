d8c5274b598ce2e00fc7bbf7ab457f8de08a2ae3
Malek Jerbi <malek.jerbi@gmail.Com>
2023-12-02 23:21:08 -0500

    assembler

--- 06/assembler/assembler.py
+++ 06/assembler/assembler.py
@@ -1,39 +1,60 @@
-# this is a one off script that assumes no errors in the input file
 from config import COMP, DEST, JUMP
+from config import SymbolTable
 
+def first_pass(filename):
+    with open(f"../{filename}/{filename}.asm", "r") as fx:
+        lineCounter = 0
+        for line in fx:
+            linetrimmed = line.strip()
+            if not linetrimmed or linetrimmed[0] == '/':
+                continue
+            if linetrimmed[0] == '(':
+                key = linetrimmed[1:-1]
+                SymbolTable[key] = lineCounter
+            else:
+                lineCounter += 1
 
+def parse_line(phrase, symbol_counter):
+    if not phrase or phrase[0:2] == '//' or phrase[0] == '(':
+        return None, symbol_counter
 
-f = open("../pong/pongL.asm", "r")
-w = open("demo.hack", "w")
-line = True
-while line:
-    line=f.readline()
-    print(line, end="")
-    if line[0:2] == '//':
-        continue
-    phrase=line.strip()
-    if not phrase:
-        continue
-
-    opcode = None
-    instruction = None
-    
     if phrase[0] == '@': 
-        opcode="0"
-        instruction=str(bin(int(phrase[1:]))[2:].zfill(15))
-        #print("opcode:", opcode, "instruction", instruction)
-        machine_code = opcode+instruction
-        print(opcode+instruction)
-        w.write(machine_code + '\n')
+        return handle_A_instruction(phrase, symbol_counter)
     else: 
-        opcode="1"
-        parts=phrase.split("=")
-        destination=parts[0] if len(parts) >= 2 else "null"
-        rest=parts[1] if len(parts) >= 2 else parts[0]
-        restparts=rest.split(";")
-        comp=restparts[0]
-        jump=restparts[1] if len(restparts) >= 2 else "null"
-        print("opcode:", opcode, "destination:", destination,  "comp:", comp, "jump:", jump)
-        machine_code = "111" + COMP[comp] + DEST[destination] + JUMP[jump]
-        print(machine_code)
-        w.write(machine_code + '\n')
+        return handle_C_instruction(phrase), symbol_counter
+
+def handle_A_instruction(phrase, symbol_counter):
+    opcode = "0"
+    x = phrase[1:]
+    try:
+        int(x)
+    except:
+        if x not in SymbolTable:
+            SymbolTable[x] = symbol_counter
+            symbol_counter += 1
+        x = SymbolTable[x]
+    instruction = str(bin(int(x))[2:].zfill(15))
+    return opcode + instruction, symbol_counter
+
+def handle_C_instruction(phrase):
+    opcode = "1"
+    parts = phrase.split("=")
+    destination = parts[0] if len(parts) >= 2 else "null"
+    rest = parts[1] if len(parts) >= 2 else parts[0]
+    restparts = rest.split(";")
+    comp = restparts[0]
+    jump = restparts[1] if len(restparts) >= 2 else "null"
+    return "111" + COMP[comp] + DEST[destination] + JUMP[jump]
+
+def assemble(filename):
+    first_pass(filename)
+    symbol_counter = 16
+    with open(f"../{filename}/{filename}.asm", "r") as f, open("demo.hack", "w") as w:
+        for line in f:
+            machine_code, symbol_counter = parse_line(line.strip(), symbol_counter)
+            if machine_code:
+                w.write(machine_code + '\n')
+
+
+filename = "pong"
+assemble(filename)
